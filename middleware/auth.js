8a57665f808e6e2657a7a3c76578288538a45deb
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-02-23 14:54:13 +0000

    adding secure user auth

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export default function (req, res, next) {
+  const token = req.headers["authorisation"];
+
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Access denied - No token provided" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid token" });
+  }
+}
